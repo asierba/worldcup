d4c4624217fc064e9076905bf040b29ef45e3b31
Asier Barrenetxea <asierba@gmail.com>
2014-06-06 19:02:30 +0100

    Playing around with routes and more stuff

---
+++ Source/WordlCup/Scripts/routes.js
@@ -0,0 +1,23 @@
+﻿angular.module('routes', ['ngRoute'])
+.config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+          controller: 'TeamCtrl',
+          templateUrl: 'templates/teams.html'
+      })
+      .when('/view/:teamId', {
+          controller: 'ViewCtrl',
+          templateUrl: 'templates/team.html'
+      })
+      .otherwise({
+          redirectTo: '/'
+      });
+})
+
+.controller('TeamCtrl', new TeamCtrl($scope))
+.controller('ViewCtrl',
+  function ($scope, $routeParams) {
+      $scope.id = $routeParams.id;
+      
+  })
+    ;
--- Source/WordlCup/Scripts/team.js
+++ Source/WordlCup/Scripts/team.js
@@ -1,7 +1,8 @@
 ﻿function TeamCtrl($scope) {
     $scope.teams = [
-        { name: "Brazil" },
-        { name: "Holland" },
-        { name: "Argentina" }
+         { name: "Brazil" },
+         { name: "Holland" },
+         { name: "Argentina" },
+          { name: "Italy" }
     ];
 }
