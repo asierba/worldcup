b05d508cda99450521a5ef18099e3c594ad015ca
Asier Barrenetxea <asierba@gmail.com>
2014-06-06 17:20:20 +0100

    All javascript 3 party libraries moved to a sub folder.

--- Source/WordlCup/App_Start/BundleConfig.cs
+++ Source/WordlCup/App_Start/BundleConfig.cs
@@ -8,19 +8,19 @@ namespace WordlCup.App_Start
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/Lib/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+                        "~/Scripts/Lib/jquery-ui-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/Lib/jquery.unobtrusive*",
+                        "~/Scripts/Lib/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/Lib/modernizr-*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
 
@@ -39,7 +39,7 @@ namespace WordlCup.App_Start
                         "~/Content/themes/base/jquery.ui.theme.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/angular").Include(
-                       "~/Scripts/angular.js")); ;
+                       "~/Scripts/Lib/angular.js")); ;
         }
     }
 }
